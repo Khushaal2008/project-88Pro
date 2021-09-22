9b0b2db3bb32fa090ee71a4ba5f0685e7f96ce24
Khushaal2008 <75684230+Khushaal2008@users.noreply.github.com>
2021-09-22 19:24:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB19hKoFLbe6aslQH04OenbBGTaMCNhAds",
+    authDomain: "project-85-baa31.firebaseapp.com",
+    projectId: "project-85-baa31",
+    storageBucket: "project-85-baa31.appspot.com",
+    messagingSenderId: "295128988595",
+    appId: "1:295128988595:web:6f145f4f00742ed7bda9c6"
+  };
